64497f7d4ceb363066ec059f9dd7eabf87c1cf6b
Caique Conde Rodrigues <condekiq@gmail.com>
2024-01-30 15:54:47 -0300

    creating classes

--- snake_game.py
+++ snake_game.py
@@ -1,15 +1,8 @@
+#---------- PACKAGES ----------
 from tkinter import *
 import random
 
-GAME_WIDTH = 700
-GAME_HEIGHT = 700
-PITCH_SIZE = 50
-SNAKE_BODY = 3
-SPEED = 50
-SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR = "#000000"
-
+#---------- CLASSES AND FUNCTIONS ----------
 class Snake:
     def __init__(self):
         self.body_size = SNAKE_BODY
@@ -17,54 +10,85 @@ class Snake:
         self.squares = []
 
         for i in range(0, SNAKE_BODY):
-            self.coordinates.append([0, 0])
+            self.coordinates.append([0, i*PITCH_SIZE])
 
         for x, y in self.coordinates:
             square = canvas.create_rectangle(x, y, x + PITCH_SIZE, y + PITCH_SIZE, fill=SNAKE_COLOR, tag="snake")
             self.squares.append(square)
-
 class Food:
     def __init__(self):
         x = random.randint(0, int((GAME_WIDTH/PITCH_SIZE)-1)) * PITCH_SIZE
         y = random.randint(0, int((GAME_HEIGHT/PITCH_SIZE)-1)) * PITCH_SIZE
         self.coordinates = [x, y]
-        canvas.create_oval(x, y, x + PITCH_SIZE, y + PITCH_SIZE, fill=FOOD_COLOR, tag="food")
+        self.squares = canvas.create_oval(x, y, x + PITCH_SIZE, y + PITCH_SIZE, fill=FOOD_COLOR, tag="food")
 
-class next_turn(snake, food):
+def next_turn(snake, food):
     x, y = snake.coordinates[0]
 
     if direction == "up":
         y -= PITCH_SIZE
-
     elif direction == "down":
         y += PITCH_SIZE
-
     elif direction == "left":
         x -= PITCH_SIZE
-
     elif direction == "right":
         x += PITCH_SIZE
 
     snake.coordinates.insert(0, (x, y))
-
     square = canvas.create_rectangle(x, y, x + PITCH_SIZE, y + PITCH_SIZE, fill=SNAKE_COLOR)
-
     snake.squares.insert(0, square)
 
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+        global score
+
+        score += 1
+
+        label.config(text="Score:{}".format(score))
+        canvas.delete("food")
+
+        food = Food()
+
+        
+
     del snake.coordinates[-1]
-
     canvas.delete(snake.squares[-1])
-
     del snake.squares[-1]
 
     window.after(SPEED, next_turn, snake, food)
 
+def change_direction(new_direction):
+
+    global direction
+
+    if new_direction == 'left' and direction != 'right':
+        direction = new_direction
+    elif new_direction == 'right' and direction != 'left':
+        direction = new_direction
+    elif new_direction == 'up' and direction != 'down':
+        direction = new_direction
+    elif new_direction == 'down' and direction != 'up':
+        direction = new_direction
+
+#---------- INITIAL VALUES ----------
+GAME_WIDTH = 500
+GAME_HEIGHT = 500
+PITCH_SIZE = 50
+SNAKE_BODY = 3
+SPEED = 100
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+score = 0
+direction = 'down'
+
+
+
+#---------- MAIN ----------
 window = Tk()
 window.title("Snake Game")
 window.resizable(False, False)
-
-score = 0
-direction = "down"
 
 label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
 label.pack()
@@ -73,6 +97,11 @@ canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDT
 canvas.pack()
 
 window.update()
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
 
 snake = Snake()
 food = Food()
