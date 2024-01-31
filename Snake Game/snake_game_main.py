6b757cf307fc01f9bdf369a0488ddb012fc37afd
Caique Conde Rodrigues <condekiq@gmail.com>
2024-01-31 13:20:48 -0300

    fixing stuff

--- Snake Game/snake_game_main.py
+++ Snake Game/snake_game_main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import random
 
+from snake_game_snake import Snake
+from snake_game_food import Food
+
 GAME_WIDTH = 500
 GAME_HEIGHT = 500
 GRID_SIZE = 50
@@ -8,30 +11,6 @@ SNAKE_SPEED = 100
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
-
-class Food:
-    def __init__(self):
-        self.color = FOOD_COLOR
-
-        x = random.randint(0, int((GAME_WIDTH/GRID_SIZE)-1)) * GRID_SIZE
-        y = random.randint(0, int((GAME_HEIGHT/GRID_SIZE)-1)) * GRID_SIZE
-        self.coordinates = [x, y]
-        self.squares = canvas.create_oval(x, y, x + GRID_SIZE, y + GRID_SIZE, fill=self.color, tag="food")
-
-class Snake:
-    def __init__(self):
-        self.coordinates = []
-        self.squares = []
-        self.color = SNAKE_COLOR
-
-        x0 = random.randint(0, int((GAME_WIDTH/GRID_SIZE)-1)) * GRID_SIZE
-        y0 = random.randint(0, int((GAME_HEIGHT/GRID_SIZE)-1)) * GRID_SIZE
-        for i in range(0, 3):
-            self.coordinates.append([x0, y0 + i*GRID_SIZE])
-
-        for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + GRID_SIZE, y + GRID_SIZE, fill=self.color, tag="snake")
-            self.squares.append(square)
 
 def next_turn(snake, food):
     x, y = snake.coordinates[0]
@@ -91,10 +70,8 @@ window.resizable(False, False)
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
-snake = Snake()
-food = Food()
-
-window.update()
+snake = Snake(canvas)
+food = Food(canvas)
 
 direction = 'down'
 window.bind('<Left>', lambda event: change_direction('left'))
@@ -103,5 +80,3 @@ window.bind('<Up>', lambda event: change_direction('up'))
 window.bind('<Down>', lambda event: change_direction('down'))
 
 next_turn(snake, food)
-
-window.mainloop()
---
+++ Snake Game/snake_game_snake.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 500
+GAME_HEIGHT = 500
+GRID_SIZE = 50
+SNAKE_SPEED = 100
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+class Snake:
+    def __init__(self, canvas):
+        self.coordinates = []
+        self.squares = []
+        self.color = SNAKE_COLOR
+
+        x0 = random.randint(0, int((GAME_WIDTH/GRID_SIZE)-1)) * GRID_SIZE
+        y0 = random.randint(0, int((GAME_HEIGHT/GRID_SIZE)-1)) * GRID_SIZE
+        for i in range(0, 3):
+            self.coordinates.append([x0, y0 + i*GRID_SIZE])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + GRID_SIZE, y + GRID_SIZE, fill=self.color, tag="snake")
+            self.squares.append(square)
